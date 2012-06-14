d0f96328637c989ce213d44f90e03f39bed4c3ee
TiniTun <caesargu@gmail.com>
2012-06-15 01:26:13 +0600

    Структура главной

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<title></title>
 	<!--link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" /-->
-	<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/less/bootstrap.less">
+	<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/less/bootstrap.less">
 	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.7.2.min.js" type="text/javascript"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/less.js" type="text/javascript"></script>
 	<?php
@@ -29,7 +29,7 @@
 ?>
 </head>
 <body>
-	<div class="navbar navbar-fixed-top">
+	<div class="navbar">
 		<div class="navbar-inner">
 			<div class="container">
 				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -37,23 +37,79 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</a>
-				<a class="brand" href="#">Project name</a>
+				<a class="brand" href="#">Рогейн в Перми</a>
 				<div class="nav-collapse">
 					<ul class="nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#contact">Contact</a></li>
+						<li class="active"><a href="#">Главная</a></li>
+						<li><a href="#about">О Рогейне</a></li>
+						<li><a href="#contact">Контакты</a></li>
 					</ul>
 				</div><!--/.nav-collapse -->
 			</div>
 		</div>
 	</div>
 
-	<div class="container">
-
-	<h1>Bootstrap starter template</h1>
-	<p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
-
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span2">
+				<div class="well sidebar-nav">
+					<ul class="nav nav-list">
+						<li class="nav-header">Соревнования</li>
+						<li><a href="#"><i class="icon-calendar"></i>Календарь</a></li>
+						<li><a href="#"><i class="icon-file"></i>Положение</a></li>
+						<li><a href="#"><i class="icon-signal"></i>Ранг рогейнеров</a></li>
+					<ul>
+				</div>
+			</div>
+			<div class="span8">
+				<div class="hero-unit">
+					<h1>Рогейн</h1>
+					<p>Рогейн (англ. Rogaining) — самостоятельный вид спорта, достаточно близкий к спортивному ориентированию, приключенческим гонкам, горным марафонам и некоторым другим видам спорта. Формально его можно считать командным суточным ориентированием по выбору, хотя в действительности он значительно отличается от классического спортивного ориентирования.</p>
+				</div>
+				<ul class="thumbnails">
+				<li class="span3">
+					<div class="thumbnail">
+						<img src="http://rogaining.ru/images/stories/news/rrc2012_logo.png" alt="">
+						<h5>9-й Чемпионат России по рогейну 2012</h5>
+						<p>Продолжается заявка на 9-й Чемпионат России по рогейну, который состоится 21 - 22 июля 2012 года в Ленинградской области. Приём заявок продлится до 10 июля 2012 г. Для оплаты стартового взноса в льготном размере нужно заявиться и оплатить взнос до 30 июня 2012 года. </p>
+						<p><a href="#" class="btn btn-primary">Подробнее</a></p>
+					</div>
+				</li>
+				<li class="span3">
+					<div class="thumbnail">
+						<img src="http://rogaining.ru/images/stories/news/rrc2012_logo.png" alt="">
+						<h5>9-й Чемпионат России по рогейну 2012</h5>
+						<p>Продолжается заявка на 9-й Чемпионат России по рогейну, который состоится 21 - 22 июля 2012 года в Ленинградской области. Приём заявок продлится до 10 июля 2012 г. Для оплаты стартового взноса в льготном размере нужно заявиться и оплатить взнос до 30 июня 2012 года. </p>
+						<p><a href="#" class="btn btn-primary">Подробнее</a></p>
+					</div>
+				</li>
+				<li class="span3">
+					<div class="thumbnail">
+						<img src="http://rogaining.ru/images/stories/news/rrc2012_logo.png" alt="">
+						<h5>9-й Чемпионат России по рогейну 2012</h5>
+						<p>Продолжается заявка на 9-й Чемпионат России по рогейну, который состоится 21 - 22 июля 2012 года в Ленинградской области. Приём заявок продлится до 10 июля 2012 г. Для оплаты стартового взноса в льготном размере нужно заявиться и оплатить взнос до 30 июня 2012 года. </p>
+						<p><a href="#" class="btn btn-primary">Подробнее</a></p>
+					</div>
+				</li>
+				<li class="span3">
+					<div class="thumbnail">
+						<img src="http://rogaining.ru/images/stories/news/rrc2012_logo.png" alt="">
+						<h5>9-й Чемпионат России по рогейну 2012</h5>
+						<p>Продолжается заявка на 9-й Чемпионат России по рогейну, который состоится 21 - 22 июля 2012 года в Ленинградской области. Приём заявок продлится до 10 июля 2012 г. Для оплаты стартового взноса в льготном размере нужно заявиться и оплатить взнос до 30 июня 2012 года. </p>
+						<p><a href="#" class="btn btn-primary">Подробнее</a></p>
+					</div>
+				</li>
+				</ul>
+			</div>
+			<div class="span2">
+				<div class="well sidebar-nav">
+					<a href="#" class="btn btn-primary">Twitter</a>
+				</div>
+				<div class="well sidebar-nav">
+					Сообщество
+				</div>
+			</div>
+		</div>
 	</div> <!-- /container -->
 </body>
 </html>
